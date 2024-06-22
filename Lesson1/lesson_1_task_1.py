648989714590c67575a2a50af8bb1f40c2ffa182
sawanekrilov1 <sawanekrilov11.11@mail.ru>
2024-06-23 01:26:31 +0300

    First Homework

---
+++ Lesson1/lesson_1_task_1.py
@@ -0,0 +1,2 @@
+my_name = "Alexandr"
+print(my_name)
---
+++ Lesson1/lesson_1_task_6.py
@@ -0,0 +1,23 @@
+def c1():
+    print("1", end="")
+
+def c2():
+    print("2", end="")    
+def c3():
+    print("3", end="")
+def c4():
+    print("4", end="")
+def c5():
+    print("5", end="")
+def c6():
+    print("6", end="")
+def c7():
+    print("7", end="")
+def c8():
+    print("8", end="")
+def c9():
+    print("9", end="")
+def c0():
+    print("0", end="")
+
+c8(),c8(),c0(),c0(),c5(),c5(),c5(),c3(),c5(),c3(),c5()
